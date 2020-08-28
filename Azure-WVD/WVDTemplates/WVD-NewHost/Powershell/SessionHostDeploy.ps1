72b872c6f17332ff010df8b2c607ad3843b9c7dd
Asif Khan <askha@microsoft.com>
2020-08-28 16:25:23 +1000

    Deploy and configure WVD Session Host vm and Domain join

--- Azure-WVD/WVDTemplates/WVD-NewHost/Powershell/SessionHostDeploy.ps1
+++ Azure-WVD/WVDTemplates/WVD-NewHost/Powershell/SessionHostDeploy.ps1
@@ -6,6 +6,13 @@ New-AzResourceGroupDeployment -Name ExampleDeployment -ResourceGroupName Example
   -TemplateFile .\WVDTemplates\WVD-NewHost\vm-from-sig\azuredeploy.json `
   -TemplateParameterFile .\WVDTemplates\WVD-NewHost\vm-from-sig\azuredeploy.parameters.json `
   -Mode Incremental 
+
+
+
+  New-AzResourceGroupDeployment -Name WVDDeployment-SIG -ResourceGroupName wvd-arm-automate `
+  -TemplateFile .\Azure-WVD\WVDTemplates\WVD-NewHost\ARM\WVD-NewHost_V2.json `
+  -TemplateParameterFile .\Azure-WVD\WVDTemplates\WVD-NewHost\ARM\WVD-NewHost.parameters.json `
+  -Mode Incremental 
   
 
   
