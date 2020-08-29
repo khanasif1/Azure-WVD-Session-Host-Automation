46598eefbdf1b4b4f55e90b46042fadd67cbb7cf
Asif Khan <askha@microsoft.com>
2020-08-29 23:37:43 +1000

    checking devops

---
+++ Azure-WVD/WVDTemplates/WVD-NewHost/Powershell/ARM_WVD.ps1
@@ -0,0 +1,18 @@
+﻿Install-Module -Name Microsoft.RDInfra.RDPowerShell
+Import-Module -Name Microsoft.RDInfra.RDPowerShell
+
+
+#https://docs.microsoft.com/en-us/powershell/module/az.desktopvirtualization/update-azwvdsessionhost?view=azps-4.6.1
+
+Install-Module -Name Az.DesktopVirtualization
+
+Connect-AzAccount
+Get-AzSubscription | Out-GridView -PassThru | Select-AzSubscription
+
+ Get-AzWvdHostPool -Name wvd-arm-dist-hp1 -ResourceGroupName wvd-arm-cross-subs
+Get-AzWvdSessionHost -HostPoolName wvd-arm-dist-hp1 -Name wvd-arm-dist-hp.wvdarm.com -ResourceGroupName wvd-arm-cross-subs
+
+Get-AzWvdUserSession -ResourceGroupName wvd-arm-cross-subs -HostPoolName wvd-arm-dist-hp1 -SessionHostName wvd-arm-dist-hp.wvdarm.com
+
+Get-AzWvdUserSession -ResourceGroupName wvd-arm-cross-subs -HostPoolName wvd-arm-dist-hp1 
+Update-AzWvdSessionHost -ResourceGroupName wvd-arm-cross-subs -HostPoolName wvd-arm-dist-hp1 -Name wvd-arm-dist-hp.wvdarm.com -AllowNewSession:$true
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Azure-WVD-Session-Host-Automation
 Azure WVD Devops project reloves around automation of hostpool session host machine management
+<a href="https://portal.azure.com/#create/Microsoft.Template/uri/https://raw.githubusercontent.com/khanasif1/Azure-WVD-Session-Host-Automation/master/Azure-WVD/WVDTemplates/WVD-NewHost/ARM/azuredeploy.json" target="_blank">
+  <img src="https://aka.ms/deploytoazurebutton"/>
+</a>
