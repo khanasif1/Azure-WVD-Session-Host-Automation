404a798f34acad9a633ffe20034be2a65881097c
Asif Khan <askha@microsoft.com>
2020-09-01 18:32:22 +1000

    cleanup of files

--- Azure-WVD/WVDTemplates/WVD-NewHost/Powershell/ARM_WVD_DrainSH.ps1
+++ Azure-WVD/WVDTemplates/WVD-NewHost/Powershell/ARM_WVD_DrainSH.ps1
@@ -3,7 +3,10 @@ param(
      [string] $hostPoolName =$(throw "hostPoolName is required.")#= "wvd-arm-dist-hp1",
     ,[string] $resourcegroupname =$(throw "resourcegroupname is required.")#= "wvd-arm-cross-subs"
 )
-foreach($sh in $(current_hp_vm)){
+
+$currenthpvm=$(current_hp_vm)
+Write-Output $currenthpvm.Count
+foreach($sh in $currenthpvm){
     Write-Output $sh
     #Update-AzWvdSessionHost -ResourceGroupName $resourcegroupname -HostPoolName $hostPoolName -Name $sh -AllowNewSession:$true
 }
